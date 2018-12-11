ce80ba13949aaf2e813fe09d6ae18dbf36dbfb52
Christopher Konopka <cskonopka@gmail.com>
2018-12-10 22:12:18 -0500

    added mkrgsm post

---
+++ Blog/04 - SayHelloArduinoGSM/00-Arduino/SayHelloArduinoGSM/SayHelloArduinoGSM.ino
@@ -0,0 +1,32 @@
+// Include the GSM library
+#include <MKRGSM.h>
+
+// initialize the library instance
+GSM gsmAccess;
+GSM_SMS sms;
+
+void setup() {
+  // Initialize serial poirt
+  Serial.begin(152000);
+
+  // Connects to the GSM network identified on the SIM card
+  gsmAccess.begin();
+  Serial.println("GSM initialized");
+}
+
+void loop() {
+  // Send Command to your server via the short code "2936"
+  // The shortcode "2936" is routed to the Commands Callback Url of your SIM
+  sms.beginSMS("2936");
+
+  // The print function is where you define the string message you would like to send
+  sms.print("hello world");
+
+  // endSMS completes the SMS command
+  sms.endSMS();
+  Serial.println("\nCOMPLETE!\n");
+
+  while (1) {
+    delay(4000);
+  }
+}
---
+++ Blog/04 - SayHelloArduinoGSM/01-Go/SayHelloArduinoGSM.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/faiface/beep"
+	"github.com/faiface/beep/mp3"
+	"github.com/faiface/beep/speaker"
+)
+
+func main() {
+	http.HandleFunc("/helloworld", helloworld)
+	http.ListenAndServe(":9999", nil)
+}
+
+func helloworld(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		log.Printf("Error parsing form: %s", err)
+		return
+	}
+
+	pwCommand := r.PostFormValue("Command")
+	fmt.Println("incoming Command from Arduino MKR GSM 1400 : ", pwCommand)
+
+	fmt.Println("Playing audio file!")
+	f, err := os.Open("hello.mp3")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s, format, _ := mp3.Decode(f)
+	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+	playing := make(chan struct{})
+	speaker.Play(beep.Seq(s, beep.Callback(func() {
+		close(playing)
+	})))
+	<-playing
+}
