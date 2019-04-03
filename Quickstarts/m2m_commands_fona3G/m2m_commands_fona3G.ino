d796c2294105c37d6da68d6e787f90f54d11ae07
TwilioIoT <cskonopka@gmail.com>
2019-04-03 12:22:05 -0400

    added comments to the m2m FONA 3G example

--- Quickstarts/m2m_commands_fona3G/m2m_commands_fona3G.ino
+++ Quickstarts/m2m_commands_fona3G/m2m_commands_fona3G.ino
@@ -15,6 +15,8 @@
 #include "Adafruit_FONA.h"
 #include <SoftwareSerial.h>
 
+
+/* Specific to the FONA 3G. */
 #define FONA_RX 2
 #define FONA_TX 3
 #define FONA_RST 4
@@ -39,7 +41,7 @@ void setup() {
   Serial.println(F("Initializing....(May take 3 seconds)"));
 
   /*
-     Adafruit Feather 32u4 FONA setup
+     Adafruit Feather FONA 3G setup.
   */
   fonaSerial->begin(4800);
   if (! fona.begin(*fonaSerial)) {
@@ -61,7 +63,7 @@ void setup() {
       Keep `command` under 160 ASCII characters, or 67 UCS-2 characters.
       https://www.twilio.com/docs/glossary/what-sms-character-limit
   */
-  char command[141] = "hello from the 3G Fona";
+  char command[141] = "hello from the FONA 3G";
 
   /*
       Write a Twilio M2M command.
@@ -76,25 +78,27 @@ void setup() {
 void loop() {
   uint16_t smslen;
   int index = 1;
+  
   /*
       Read a Twilio M2M command. Note that it will find the lowest
       indexed one with the code as is; in your code. if you cache the
       index you can start the next read_command to move to the next one.
   */
-
   if (!fona.readSMS(index, replybuffer, 250, &smslen)) {
     Serial.println(F("Failed!"));
   } else {
 
-    /* Print the command */
+    /* Print the command. */
     SerialUSB.println(replybuffer);
     delay(2000);
 
-    /* Delete the stored command */
+    /* Delete the stored command. */
     fona.deleteSMS(1);
 
     /* That's all, folks! */
     Serial.println("Finished.");
+
+    /* Send a final sign-off command. */
     if (!fona.sendSMS("2936", "signing off")) {
       Serial.println(F("Failed"));
     } else {
